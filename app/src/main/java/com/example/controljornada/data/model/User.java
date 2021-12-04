93da2f1c26c4e2e7b8c36784d7299fbda95fbfc4
pabloinfantes <pabloinfantes@gmail.com>
2021-12-04 11:12:42 +0100

    4

---
+++ app/src/main/java/com/example/controljornada/data/model/User.java
@@ -0,0 +1,13 @@
+package com.example.controljornada.data.model;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+public class User implements Serializable {
+    String nombre;
+    String apellidos;
+    String genero;
+    String telefono;
+    int edad;
+
+}
---
+++ app/src/main/java/com/example/controljornada/ui/profile/ProfileFragment.java
@@ -0,0 +1,42 @@
+package com.example.controljornada.ui.profile;
+
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.controljornada.R;
+import com.example.controljornada.data.model.Ajustes;
+import com.example.controljornada.databinding.FragmentProfileBinding;
+
+
+public class ProfileFragment extends Fragment {
+
+    FragmentProfileBinding binding;
+
+    String[] generos = {"Seleccione","Femenino","Masculino"};
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = FragmentProfileBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding.spinner.setAdapter(new ArrayAdapter<String>(getActivity(), R.layout.support_simple_spinner_dropdown_item,generos));
+    }
+}
